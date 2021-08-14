743290c854293937f64aa2c282077d013476e994
thaidang111 <88382936+thaidang111@users.noreply.github.com>
2021-08-14 10:10:33 +0700

    Update crush.js

--- pages/crush.js
+++ pages/crush.js
@@ -20,12 +20,7 @@ function Crush() {
     <div className="crush-box">
       <div className="title">
         <div>
-          Tuấn có
-          <div className="color-porn-hub">
-            <div> đẹp </div>
-            <div> trai </div>
-          </div>
-          không?
+          Thái có ĐẸP TRAI không?
         </div>
       </div>
       <button className="btn-click-me btn-yes" onClick={onClickYes}>
